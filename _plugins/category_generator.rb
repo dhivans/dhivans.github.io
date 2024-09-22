387fb7843fa40559f36b60bc73a509e33e6115a8
dhivans <154624951+dhivans@users.noreply.github.com>
2024-09-22 22:18:51 +0100

    Update category_generator.rb

--- _plugins/category_generator.rb
+++ _plugins/category_generator.rb
@@ -1,30 +1,35 @@
 module Jekyll
-    class ItemCategoryPage < Page
-      def initialize(site, base, dir, category)
-        @site = site
-        @base = base
-        @dir = dir
-        @name = 'index.html'
-  
-        self.process(@name)
-        self.read_yaml(File.join(base, '_layouts'), 'category-items.html')
-        self.data['category'] = category
-        self.data['title'] = "Category: #{category.capitalize}"
-      end
+  class ItemCategoryPage < Page
+    def initialize(site, base, dir, category)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'index.html'
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'category-items.html')
+      self.data['category'] = category
+      self.data['title'] = "Category: #{category.capitalize}"
     end
-  
-    class ItemCategoryGenerator < Generator
-      safe true
-  
-      def generate(site)
-        if site.collections.key?('items')
-          categories = site.collections['items'].docs.flat_map { |doc| doc.data['categories'] }.uniq
-          
-          categories.each do |category|
-            site.pages << ItemCategoryPage.new(site, site.source, File.join('categories/items', category.downcase), category)
-          end
+  end
+
+  class ItemCategoryGenerator < Generator
+    safe true
+
+    def generate(site)
+      if site.collections.key?('items') && site.posts.docs.any?
+        # Gather all post categories
+        post_categories = site.posts.docs.flat_map { |post| post.data['categories'] }.uniq
+
+        # Gather item categories and filter them
+        item_categories = site.collections['items'].docs.flat_map { |doc| doc.data['categories'] }.uniq
+        filtered_categories = item_categories.reject { |category| post_categories.include?(category) }
+        
+        # Create pages only for item categories not in post categories
+        filtered_categories.each do |category|
+          site.pages << ItemCategoryPage.new(site, site.source, File.join('categories', category.downcase), category)
         end
       end
     end
   end
-  
+end
